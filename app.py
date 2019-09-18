0e158dcfc95c8095b6c89ca2809cd6c3a0b7bbe0
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2019-09-17 22:36:08 -0700

    Now able to request gif queries

--- app.py
+++ app.py
@@ -11,23 +11,35 @@ def index():
 
     # TODO: Make 'params' dictionary containing:
     # a) the query term, 'q'
+    query = request.args.get('query')
     # b) your API key, 'key'
-    # c) how many GIFs to return, 'limit'
+    apikey = "JWEOV1L8X0N2"
 
-    # TODO: Make an API call to Tenor using the 'requests' library. For 
-    # reference on how to use Tenor, see: 
-    # https://tenor.com/gifapi/documentation
+    # c) how many GIFs to return, 'limit'
+    lmt = 9
+
+    # params dictionary
+    params = {
+        "q": query,
+        "key": apikey,
+        'limit': lmt
+        }
 
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
-
+    r = requests.get("https://api.tenor.com/v1/search", params)
+    
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
-
+    if r.status_code == 200:
+        gifs = json.loads(r.content)['results']
+    else:
+        gifs = None
+    
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
-
-    return render_template("index.html")
+    
+    return render_template("index.html", gifs = gifs)
 
 if __name__ == '__main__':
     app.run(debug=True)
